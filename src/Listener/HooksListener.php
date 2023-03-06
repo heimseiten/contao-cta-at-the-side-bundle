9b9bd77c3fac4c01c5eecac9298e3530532b895b
Niels <info@heimseiten.de>
2023-03-06 11:37:32 +0100

    Update HooksListener.php

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -15,22 +15,19 @@ class HooksListener
 
     private function processBuffer(string $buffer, $templateName): string
     {
-        if (TL_MODE == 'FE') 
-        {
-            global $objPage;
+        global $objPage;
 
-            $html_strukutur = '';
-            $rootPage = PageModel::findById($GLOBALS['objPage']->rootId);
-            if ($rootPage->cta_position) {
-                if ($rootPage->cta_1_link) { $html_strukutur .= generateHTML($rootPage->cta_1, $rootPage->cta_1_text, $rootPage->cta_1_link); }
-                if ($rootPage->cta_2_link) { $html_strukutur .= generateHTML($rootPage->cta_2, $rootPage->cta_2_text, $rootPage->cta_2_link); }
-                if ($rootPage->cta_3_link) { $html_strukutur .= generateHTML($rootPage->cta_3, $rootPage->cta_3_text, $rootPage->cta_3_link); }
-                if ($rootPage->cta_4_link) { $html_strukutur .= generateHTML($rootPage->cta_4, $rootPage->cta_4_text, $rootPage->cta_4_link); }
-            }
-            if ($html_strukutur) { $html_strukutur = '<div class="cta_wrapper"><div class="inside ' . $rootPage->cta_position . '">' . $html_strukutur . '</div></div>'; }
-
-            $buffer = preg_replace('/<div id="wrapper">/', '<div id="wrapper">' . $html_strukutur, $buffer, 1);   
+        $html_strukutur = '';
+        $rootPage = PageModel::findById($GLOBALS['objPage']->rootId);
+        if ($rootPage->cta_position) {
+            if ($rootPage->cta_1_link) { $html_strukutur .= generateHTML($rootPage->cta_1, $rootPage->cta_1_text, $rootPage->cta_1_link); }
+            if ($rootPage->cta_2_link) { $html_strukutur .= generateHTML($rootPage->cta_2, $rootPage->cta_2_text, $rootPage->cta_2_link); }
+            if ($rootPage->cta_3_link) { $html_strukutur .= generateHTML($rootPage->cta_3, $rootPage->cta_3_text, $rootPage->cta_3_link); }
+            if ($rootPage->cta_4_link) { $html_strukutur .= generateHTML($rootPage->cta_4, $rootPage->cta_4_text, $rootPage->cta_4_link); }
         }
+        if ($html_strukutur) { $html_strukutur = '<div class="cta_wrapper"><div class="inside ' . $rootPage->cta_position . '">' . $html_strukutur . '</div></div>'; }
+
+        $buffer = preg_replace('/<div id="wrapper">/', '<div id="wrapper">' . $html_strukutur, $buffer, 1);   
         return $buffer;   
     }
 }

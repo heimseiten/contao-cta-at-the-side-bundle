979a967213133a2b6ffac0dc2b396b8dde00e505
Niels <info@heimseiten.de>
2023-03-06 13:35:07 +0100

    Update HooksListener.php

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Heimseiten\ContaoCtaAtTheSideBundle\Listener;
 
 use Contao\PageModel;
+use Contao\System;
 
 class HooksListener
 {
@@ -20,10 +21,10 @@ class HooksListener
         $html_strukutur = '';
         $rootPage = PageModel::findById($GLOBALS['objPage']->rootId);
         if ($rootPage->cta_position) {
-            if ($rootPage->cta_1_link) { $html_strukutur .= generateHTML($rootPage->cta_1, $rootPage->cta_1_text, $rootPage->cta_1_link); }
-            if ($rootPage->cta_2_link) { $html_strukutur .= generateHTML($rootPage->cta_2, $rootPage->cta_2_text, $rootPage->cta_2_link); }
-            if ($rootPage->cta_3_link) { $html_strukutur .= generateHTML($rootPage->cta_3, $rootPage->cta_3_text, $rootPage->cta_3_link); }
-            if ($rootPage->cta_4_link) { $html_strukutur .= generateHTML($rootPage->cta_4, $rootPage->cta_4_text, $rootPage->cta_4_link); }
+            if ($rootPage->cta_1_link) { $html_strukutur .= generateHTML( $rootPage->cta_1, $rootPage->cta_1_text, System::getContainer()->get('contao.insert_tag.parser')->replace($rootPage->cta_1_link) ); }
+            if ($rootPage->cta_2_link) { $html_strukutur .= generateHTML( $rootPage->cta_2, $rootPage->cta_2_text, System::getContainer()->get('contao.insert_tag.parser')->replace($rootPage->cta_2_link) ); }
+            if ($rootPage->cta_3_link) { $html_strukutur .= generateHTML( $rootPage->cta_3, $rootPage->cta_3_text, System::getContainer()->get('contao.insert_tag.parser')->replace($rootPage->cta_3_link) ); }
+            if ($rootPage->cta_4_link) { $html_strukutur .= generateHTML( $rootPage->cta_4, $rootPage->cta_4_text, System::getContainer()->get('contao.insert_tag.parser')->replace($rootPage->cta_4_link) ); }
         }
         if ($html_strukutur) { $html_strukutur = '<div class="cta_wrapper"><div class="inside ' . $rootPage->cta_position . '">' . $html_strukutur . '</div></div>'; }
 

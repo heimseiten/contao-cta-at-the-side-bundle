38b83a26c625aa0b8fc2562d8a8f43389b4d1c68
Niels <info@heimseiten.de>
2023-06-09 13:17:20 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -6,12 +6,12 @@ $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoctaattheside/cta-at-the-side.scs
 
 function generateHTML($cta_image, $cta_text, $cta_link)
 {
-    $html_strukutur .= '<div class="link_wrapper"><a href="' . $cta_link . '">';
+    $html_strukutur .= '<div class="link_wrapper"><div class="inside"><a href="' . $cta_link . '">';
     
     if (generateImageTag($cta_image)) { $html_strukutur .= generateImageTag($cta_image); }
     if ($cta_text) { $html_strukutur .= '<p>' . $cta_text . '</p>'; }
     
-    $html_strukutur .= '</a></div>';
+    $html_strukutur .= '</a></div></div>';
 
     return $html_strukutur;
 }

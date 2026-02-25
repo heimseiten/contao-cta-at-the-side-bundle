70163030869cb3403687743ebe3cfd779d6b7124
Niels <info@heimseiten.de>
2026-02-25 15:18:20 +0100

    Refactor generateHTML and generateImageTag functions

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,25 +1,50 @@
 <?php
 
 use Contao\FilesModel;
+use Contao\StringUtil;
 
 $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoctaattheside/cta-at-the-side.scss|static';
 
 function generateHTML($cta_image, $cta_text, $cta_link)
 {
+    $html_strukutur = '';
     $html_strukutur .= '<div class="link_wrapper"><div class="inside"><a href="' . $cta_link . '">';
-    
-    if (generateImageTag($cta_image)) { $html_strukutur .= generateImageTag($cta_image); }
-    if ($cta_text) { $html_strukutur .= '<p>' . $cta_text . '</p>'; }
-    
+
+    $imgTag = generateImageTag($cta_image);
+    if ($imgTag) {
+        $html_strukutur .= $imgTag;
+    }
+
+    if ($cta_text) {
+        $html_strukutur .= '<p>' . $cta_text . '</p>';
+    }
+
     $html_strukutur .= '</a></div></div>';
 
     return $html_strukutur;
 }
 
-function generateImageTag($cta_image) 
+function generateImageTag($cta_image)
 {
-    if ($cta_image) 
-    { 
-        return '<img src="' . FilesModel::findByUuid($cta_image)->path . '">'; 
+    if ($cta_image) {
+        $file = FilesModel::findByUuid($cta_image);
+        if ($file === null) {
+            return '';
+        }
+
+        $src = StringUtil::specialchars($file->path);
+
+        // Take alt text ONLY from file meta data (language-aware); otherwise keep alt=""
+        $meta = StringUtil::deserialize($file->meta, true);
+        $lang = $GLOBALS['TL_LANGUAGE'] ?? 'en';
+
+        $alt = '';
+        if (is_array($meta) && isset($meta[$lang]) && is_array($meta[$lang]) && array_key_exists('alt', $meta[$lang])) {
+            $alt = trim((string) $meta[$lang]['alt']);
+        }
+
+        return '<img src="' . $src . '" alt="' . StringUtil::specialchars($alt) . '">';
     }
+
+    return '';
 }

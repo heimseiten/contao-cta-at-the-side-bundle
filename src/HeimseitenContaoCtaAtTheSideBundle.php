e317a869819ad3a386dc31190da8f5f897836721
Niels <info@heimseiten.de>
2026-06-23 11:29:23 +0200

    Modernize bundle layout (AbstractBundle, resources at root); remove boilerplate tests

--- src/HeimseitenContaoCtaAtTheSideBundle.php
+++ src/HeimseitenContaoCtaAtTheSideBundle.php
@@ -2,32 +2,16 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of call to action at the side of the page.
- * 
- * (c) heimseiten.de - Webdesign aus Köln 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-cta-at-the-side-bundle
- */
-
 namespace Heimseiten\ContaoCtaAtTheSideBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-/**
- * Class HeimseitenContaoCtaAtTheSideBundle
- */
-class HeimseitenContaoCtaAtTheSideBundle extends Bundle
+class HeimseitenContaoCtaAtTheSideBundle extends AbstractBundle
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function build(ContainerBuilder $container): void
-	{
-		parent::build($container);
-		
-	}
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
